ac796592f569afc31a0168799770b53b032ebf85
Acuiza <Acuiza@hypernovalabs.com>
2025-08-11 14:56:35 -0400

    delete console and imports

--- app/services/ServiceCategories/index.tsx
+++ app/services/ServiceCategories/index.tsx
@@ -1,14 +1,12 @@
-import Category from "@/app/helper/Category";
 
 const URLCategories = 'https://api.chucknorris.io/jokes/categories';
-const getCategories = async (): Promise<Category[]> => {
+const getCategories = async (): Promise<string[]> => {
     const response = fetch(URLCategories).then((response) => {
         if (!response.ok) {
             throw new Error('Network Error response get Categories');
         }
         return response.json();
     });
-    console.log("data",response);
    return response;
 }
 export default getCategories;
