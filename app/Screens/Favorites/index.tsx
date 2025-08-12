23ff0326c70b25023fd700d0239ecb9121e6b8c6
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 16:56:15 -0400

    get favorites Offline  when is offline

--- app/Screens/Favorites/index.tsx
+++ app/Screens/Favorites/index.tsx
@@ -1,6 +1,7 @@
 import FavoriteViewModel from "@/app/ModelViewModels/FavoriteViewModel";
 import { useAuthStore } from "@/app/Store/Auth";
 import { useFavoriteStore } from "@/app/Store/Favorites";
+import { useOfflineStore } from "@/app/Store/Offline";
 import ListFavorites from "@/components/ListFavorites";
 import { useEffect } from "react";
 import { ActivityIndicator } from "react-native-paper";
@@ -8,12 +9,16 @@ import { ActivityIndicator } from "react-native-paper";
 const Favorites = () => {
   const {favorites, loading} = useFavoriteStore()
   const {user} = useAuthStore()
-  const {getAllFavorites,removeAFavorite } = FavoriteViewModel();
+  const {getAllFavorites,removeAFavorite,getFavoritesOffline} = FavoriteViewModel();
+  const {isOffline} = useOfflineStore();
   useEffect(()=>{
-    if(user) {
+    if(user && !isOffline) {
       getAllFavorites(user)
     }
-  },[])
+    if(isOffline) {
+      getFavoritesOffline()
+    }
+  },[user,isOffline])
   if (loading) {
     return  <ActivityIndicator size="large" style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} />
   }
