ad6a2610203af33376279183ca2b91869271ad5a
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 16:57:36 -0400

    chnage state IsOffline from zuntand when detect that i have not conection to internet

--- components/ConnectivityToast/index.tsx
+++ components/ConnectivityToast/index.tsx
@@ -1,9 +1,11 @@
+import { useOfflineStore } from "@/app/Store/Offline";
 import NetInfo from "@react-native-community/netinfo";
 import { useEffect, useState } from "react";
 import { Portal, Snackbar } from "react-native-paper";
 
 const ConnectivityToast = () => {
   const [isNotConnected, setIsNotConnected] = useState(false);
+  const {setIsOffline} = useOfflineStore();
   useEffect(() => {
     let mounted = true;
     NetInfo.fetch().then((state) => {
@@ -12,12 +14,14 @@ const ConnectivityToast = () => {
       }
       const isOffline = state.isConnected === false;
       setIsNotConnected(isOffline);
+      setIsOffline(isOffline);
     });
     const unsubscribe = NetInfo.addEventListener((state) => {
       const isOffline =
         state.isConnected === false;
     
       setIsNotConnected(isOffline);
+      setIsOffline(isOffline);
     });
     return () => {
       mounted = false;
