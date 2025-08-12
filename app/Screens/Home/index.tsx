6c0739d31d2a15397c6c72ed507a9a612a9e71af
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 14:39:38 -0400

    add state loading and set loading when get categories and show loading in the screen

--- app/Screens/Home/index.tsx
+++ app/Screens/Home/index.tsx
@@ -3,15 +3,23 @@ import ModelViewModel from "@/app/ModelViewModels/HomeViewModel";
 import { useCategoryStore } from "@/app/Store/Categories";
 import ListCategories from "@/components/ListCategories";
 import { useEffect } from "react";
+import { ActivityIndicator } from "react-native-paper";
 
 const Home = (props) => {
   const {fetchCategories} = ModelViewModel();
-  const {categories} = useCategoryStore();
+  const {categories, loading} = useCategoryStore();
   useEffect(() => {
     fetchCategories();
   }, []);
   const navigationToScreenJokes = (category: string) => {
     props.navigation.navigate(NameScreens.Details, { category });
+  }
+  if(loading) {
+    return (
+      <>
+      <ActivityIndicator size="large" style={{ flex: 1, justifyContent: "center", alignItems: "center" }} />
+      </>
+    )
   }
   return (
     <>
