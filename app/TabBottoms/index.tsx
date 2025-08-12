0f7d711d7201f41703dd15b47c065b968bd8dcaf
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 13:02:35 -0400

    add screen Search

--- app/TabBottoms/index.tsx
+++ app/TabBottoms/index.tsx
@@ -1,9 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Ionicons from "@expo/vector-icons/Ionicons";
-import Favorites from "../Screens/Favorites";
-import HomeStack from "../StackScreen/HomeStack";
 import { NameScreens } from "../helper/values";
+import Favorites from "../Screens/Favorites";
+import Search from "../Screens/Search";
+import HomeStack from "../StackScreen/HomeStack";
 const TabBottoms = () => {
     const Tab = createBottomTabNavigator();
 
@@ -17,6 +18,8 @@ const TabBottoms = () => {
                     iconName = focused ? 'home' : 'home-outline';
                 } else if (route.name === NameScreens.Favorites) {
                     iconName = focused ? 'heart' : 'heart-outline';
+                } else if (route.name === NameScreens.Search) {
+                    iconName = focused ? 'search' : 'search-outline';
                 }
                 return <Ionicons name= {iconName} size={size} color={color} />;
             },
@@ -26,6 +29,8 @@ const TabBottoms = () => {
         initialRouteName={NameScreens.Home}>
             <Tab.Screen name={NameScreens.Home} component={HomeStack} />
             <Tab.Screen name={NameScreens.Favorites} component={Favorites} />
+            <Tab.Screen name={NameScreens.Search} component={Search}/>
+
         </Tab.Navigator>
 
     )
