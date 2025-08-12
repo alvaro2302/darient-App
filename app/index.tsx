28fd1f3bbee26762a9f09ed245da0d08cc8348c2
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 18:52:12 -0400

    call function initializeNotification for give permision when init the App

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import ConnectivityToast from '@/components/ConnectivityToast';
+import { initializeNotification } from '@/lib/notification';
 import { PaperProvider } from 'react-native-paper';
 import 'react-native-reanimated';
 import InitialStack from './StackScreen/InitialStack';
 
 
 export default function RootLayout() {
-  
+  initializeNotification();
   return (
     <PaperProvider>
       <ConnectivityToast/>
