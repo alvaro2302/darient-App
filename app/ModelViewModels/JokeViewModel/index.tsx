61692fe73e224c09685c5d573bd372dcd921605e
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 01:45:21 -0400

    add function for get a Joke

---
+++ app/ModelViewModels/JokeViewModel/index.tsx
@@ -0,0 +1,23 @@
+import getJokeRandom from "@/app/services/ServiceJoke";
+import { useJokeStore } from "@/app/Store/Joke";
+const JokeViewModel = () => {
+  const { setLoading, saveJoke } = useJokeStore();
+  const getJoke = async (categoryJoke: string) => {
+    try {
+      setLoading(true);
+      const joke = await getJokeRandom(categoryJoke);
+      const jokeData = {
+        id: joke.id,
+        value: joke.value,
+      };
+      saveJoke(jokeData);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error fetching joke:", error);
+    }
+  };
+  return {
+    getJoke,
+  };
+};
+export default JokeViewModel;
