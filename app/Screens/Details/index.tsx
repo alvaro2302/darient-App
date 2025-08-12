0d0be3251f4e92288d649f78464e87473dd88745
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 11:30:50 -0400

    refactoring in a component called JokeComponent

--- app/Screens/Details/index.tsx
+++ app/Screens/Details/index.tsx
@@ -2,41 +2,37 @@ import JokeViewModel from "@/app/ModelViewModels/JokeViewModel";
 import { addFavorite } from "@/app/services/ServiceFavorite";
 import { useAuthStore } from "@/app/Store/Auth";
 import { useJokeStore } from "@/app/Store/Joke";
-import FavoriteHeart from "@/components/FavoriteHeart";
+import JokeComponent from "@/components/Joke";
 import { useEffect } from "react";
 import { View } from "react-native";
-import { ActivityIndicator, Card, Text } from "react-native-paper";
+import { ActivityIndicator } from "react-native-paper";
 interface DetailsProps {
   route: { key: string; params: { category: string } };
   navigation: any;
 }
 const DetailsJoke = (props: DetailsProps) => {
-  const { getJoke} = JokeViewModel();
-  const {joke,loading} = useJokeStore()
+  const { getJoke } = JokeViewModel();
+  const { joke, loading } = useJokeStore();
   const { category } = props.route.params;
-  const {user} = useAuthStore();
+  const { user } = useAuthStore();
   useEffect(() => {
     getJoke(category);
-  },[]);
+  }, []);
 
-  if(loading) {
-    return <ActivityIndicator size="large" style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} />
+  if (loading) {
+    return (
+      <ActivityIndicator
+        size="large"
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      />
+    );
   }
   return (
     <View>
-      { joke && user &&(
-        <Card style={{alignSelf: 'center', margin: 20, width: '90%'}}> 
-          <Card.Title title={'Chuck Norris Say:'} right={(props) => <FavoriteHeart joke={joke} userLogged={user} addFavorite={addFavorite}/>}
-          
-          />
-          
-          <Card.Content>
-            <Text variant='titleMedium'>{joke.value}</Text>
-          </Card.Content>
-          
-        </Card>)
-    }
+      {joke && user && (
+        <JokeComponent joke={joke} user={user} addFavorite={addFavorite} />
+      )}
     </View>
   );
-}
+};
 export default DetailsJoke;
