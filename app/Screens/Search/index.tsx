b2e53a270ea3819f8f21768a4d1a6e0543acd9e0
Acuiza <Acuiza@hypernovalabs.com>
2025-08-12 14:32:04 -0400

    add component ListResultSearch in the screen

--- app/Screens/Search/index.tsx
+++ app/Screens/Search/index.tsx
@@ -1,14 +1,15 @@
 import SearchViewModel from "@/app/ModelViewModels/SearchViewModel";
 import { useJokeSearchStore } from "@/app/Store/JokeSearch";
-import { FlatList, View } from "react-native";
-import { ActivityIndicator, Card, Searchbar, Text } from "react-native-paper";
+import ListResultSearch from "@/components/ListResultSearch";
+import { View } from "react-native";
+import { Searchbar } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 const Search = () => {
   const { searchJokeSpecific } = SearchViewModel();
   const { search, setSearch, resultSearch, loading } = useJokeSearchStore();
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{width:'100%'}} >
+      <View style={{flex:1}} >
         <View style={{marginHorizontal:'2%',paddingBottom:40}}>
           <Searchbar
             placeholder="Search"
@@ -17,27 +18,7 @@ const Search = () => {
             onSubmitEditing={async () => searchJokeSpecific(search)}
           />
         </View>
-        {loading && (
-          <ActivityIndicator
-            size="large"
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          />
-        )}
-        {resultSearch && !loading && (
-          <FlatList
-            data={resultSearch.result}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({ item }) => (
-              <Card style={{ alignSelf: "center", margin: 20, width: "90%" }}>
-                <Card.Title title={"Chuck Norris Say:"} />
-
-                <Card.Content>
-                  <Text variant="titleMedium">{item.value}</Text>
-                </Card.Content>
-              </Card>
-            )}
-          />
-        )}
+        <ListResultSearch listSearch={resultSearch?.result || []} loading={loading} />
       </View>
     </SafeAreaView>
   );
